f4d50481632483904181d029f4fba461ac2b346a
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-03 21:25:07 -0400

    Update

---
+++ day3.js
@@ -0,0 +1,34 @@
+
+mystring =  `
+
+<h1 id = "day3">Day 3 </h1>
+
+
+<h2 id = "day3-immunity">Immunity Challenge </h2>
+Taleitaki has won immunity, which means Kalokalo is going to tribal!
+
+
+<h2 id = "day3-status"> Kalokalo status</h2>
+
+<p>Kalokalo is sad to have lost, since everybody likes each other and doesn’t want to vote anybody out. However, Spencer, Jason, Aubry, Scot, Devon, Zeke, Tai, and Christian decide that either Ben or Tony need to be voted out, and will split the vote between them. Ben and Tony start scrambling and Ben goes on an idol hunt. He searches for a while, until he see something by a tree stump…it’s the idol! Ben is joyous to have found it, and tells Tony immediately. However, Ben wants to save it so him and Tony do some work to try to change the vote. They start talking to Jason, proposes a plan to vote out Tai, since they know Jason has bad memories with Tai. Jason tells Scot, who is also a fan of the idea. However, Zeke heres them talking about voting out Tai, and runs and tells Tai he might be in trouble. Tai gets paranoid and starts trying to find an idol, unaware that Ben already has it. He then starts attempting to get the numbers attempting to flip on Jason and Scot by voting Scot out. </p>
+
+<p>With tons of targets, this is bound to be an interesting first vote.</p>
+
+<h2 id = "day3-tribal"> Tribal Council </h2>
+
+<p>
+ Jeff: Behind each of you is a torch. Go ahead and grab a torch and approach the flame. Dip it in and get fire. This is a part of the ritual of tribal council, because in this game, fire represents life. As long as you have fire, you are still in this game. When your fire's gone, so are you.
+</p>
+
+<p>
+Short recap:
+</p>
+
+<p>The majority 8 planned to split the vote between Tony and Ben, the two winners and biggest threats on the tribe. However, Ben found an idol, which means, if played correctly, could save them. They tried to convince Jason and Scot to vote Tai since they have bad experiences with him. However, Zeke overheard this conversation, and told Tai, who then targeted Scot.
+With so many targets, it is bound to be a crazy tribal council.
+Alright, it is time to vote.</p>
+
+`;
+
+
+writeDay(mystring);
