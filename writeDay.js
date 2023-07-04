e9fcb73bf84756a4804b698672b1f80487a8583e
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-04 11:23:49 -0400

    Update

--- writeDay.js
+++ writeDay.js
@@ -1,6 +1,6 @@
 function writeDay(s) {
     document.write('<div class="row">');
-    document.write('<div class="col-lg-8 col-lg-offset-2">');         
+    document.write('<div class="col-8 col-offset-2">');         
     document.write(s);  
     document.write('<hr style = "background-color:darkred; height:1px">');
     document.write('</div>');    
