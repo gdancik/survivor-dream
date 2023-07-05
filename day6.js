5e802a3e7a2a946b3a4f58f79e0a8273ffa9f7ab
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-05 17:18:47 -0400

    Update

---
+++ day6.js
@@ -0,0 +1,29 @@
+
+mystring =  `
+
+<h1 id = "day6">Day 6 </h1>
+
+
+<h2 id = "day6-immunity">Immunity Challenge </h2>
+
+<p>Taleitaki won again, meaning Kalokalo is going back to tribal council!</p>
+
+<h2 id = "day6-status"> Kalokalo status</h2>
+
+<p> After getting back from the immunity challenge, Kalokalo is bummed that they lost. However, Tony, Ben, Scot, and Jason all feel good since they have the numbers with Spencer and all they have to do is pick off one of the 4 in the minority. Devon and Christian decide that they should just stick together and hope that the majority splits, so they want to keep it simple and put their votes on Tony. However, Zeke is not going to give up that easily. He goes up to the majority alliance and tries to convince them to put their votes on Aubry, since she is a huge strategic threat. Aubry also doesn’t want to give up - and she realizes that Spencer isn’t fully on board with the alliance. She goes to have a talk with him and tries to convince him that they can be a duo. Spencer wants to work with Aubry, but he’s not sure if he should flip yet. Meanwhile, Ben, Tony, Jason, and Scot realize that Spencer has been talking to Aubry for a long time and they consider putting votes on him also. Christian and Devon also realize that everybody else is talking, and they get worried that either they are throwing them under the bus or telling them there target. So Christian and Devon consider changing the vote to Ben, or also maybe Zeke since they know Zeke likes to play hard and might be throwing them under the bus.</p>
+
+<p>With a ton more crazy things happening, this is going to be another crazy tribal.</p>
+
+
+<h2 id = "day6-tribal"> Tribal Council </h2>
+
+<h3> Summary </h3>
+
+<h2 id = 'day6-status2'>Status </h2>
+
+
+
+`;
+
+
+writeDay(mystring);
