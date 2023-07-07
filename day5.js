d3afd697f7f3edc43176bde123d929e65e4b25af
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-07 17:25:49 -0400

    Update

---
+++ day5.js
@@ -0,0 +1,65 @@
+
+mystring =  `
+
+<h1 id = "day5">Day 5 </h1>
+
+
+<h2 id = "day5-immunity">Immunity Challenge </h2>
+
+<p>Taleitaki won again, meaning Kalokalo is going back to tribal council!</p>
+
+<h2 id = "day5-status"> Kalokalo status</h2>
+
+<p> After getting back from the immunity challenge, Kalokalo is bummed that they lost. However, Tony, Ben, Scot, and Jason all feel good since they have the numbers with Spencer and all they have to do is pick off one of the 4 in the minority. Devon and Christian decide that they should just stick together and hope that the majority splits, so they want to keep it simple and put their votes on Tony. However, Zeke is not going to give up that easily. He goes up to the majority alliance and tries to convince them to put their votes on Aubry, since she is a huge strategic threat. Aubry also doesn’t want to give up - and she realizes that Spencer isn’t fully on board with the alliance. She goes to have a talk with him and tries to convince him that they can be a duo. Spencer wants to work with Aubry, but he’s not sure if he should flip yet. Meanwhile, Ben, Tony, Jason, and Scot realize that Spencer has been talking to Aubry for a long time and they consider putting votes on him also. Christian and Devon also realize that everybody else is talking, and they get worried that either they are throwing them under the bus or telling them there target. So Christian and Devon consider changing the vote to Ben, or also maybe Zeke since they know Zeke likes to play hard and might be throwing them under the bus.</p>
+
+<p>With a ton more crazy things happening, this is going to be another crazy tribal.</p>
+
+
+
+<h2 id = "day5-tribal"> Tribal Council </h2>
+
+<h3> Recap </h3>
+
+<p>Devon, Christian, Zeke, and Aubry were in the minority, planning to vote for Tony and hope something happened. However, Zeke talked to the majority and tried to throw Aubry under the bus. Meanwhile, Aubry was forming a bond with Spencer to try to get him to flip. Ben, Tony, Jason, and Scot realized Spencer was gone for a long time and were considering changing the vote to him. Then, Devon and Christian got paranoid and thought that either Zeke told the majority their plan or threw them under the bus. So they thought about flipping to vote out Ben or Zeke.</p>
+
+<h3> The Votes </h3>
+
+<p>I'll go tally the votes. </p>
+
+<p>If anybody has an immunity idol and wants to play it, now would be the time to do so.</p>
+
+<p>Alright. I'll read the votes.</p>
+
+<p>Aubry</p>
+
+<p>Tony</p>
+
+<p>Spencer</p>
+
+<p>Zeke</p>
+
+<p>Spencer. That's two votes Spencer, one vote Aubry, one vote Tony, and one vote Zeke.</p>
+
+<p>Zeke. That's two votes Zeke, two votes Spencer, one vote Tony and Aubry.</p>
+
+<p>Zeke. </p>
+
+
+<p>Zeke. </p>
+
+<p>Second person voted out of Survivor: Dream All Stars.... Zeke.</p>
+
+<p>
+<img src = "imgs/votes-day-5.png"></img>
+</p></br>
+
+
+
+<h2 id = 'day5-status2'>Status </h2>
+
+
+
+`;
+
+
+writeDay(mystring);
--- day6.js
+++ day6.js
@@ -4,59 +4,69 @@ mystring =  `
 <h1 id = "day6">Day 6 </h1>
 
 
+<h2 id = "day6-events">Tribe Swap </h2>
+
+ <p>Alright everybody...drop your buffs! We're switching tribes! We are going from two tribes to three.</p>
+
+<p>If you have the blue buff, you are on Kalokalo. If you have the red buff, you are on Taleitaki.  And if you have the green buff, you are on the new tribe, Yavusa Tolu (Fijian for tribe 3 lol).</p>
+
+<p>Here are the new tribes:</p>
+
+<h3>Kalokalo</h3>
+<ol>
+<li>Brad Culpepper</li>
+    <li>David Wright</li>
+    <li>Ben Driebergen</li>
+    <li>Michaela Bradshaw</li>
+    <li>Domenick Abbate</li>
+    <li>Ozzy Lusth</li>
+</ol>
+
+<h3>Taleitaki</h3>
+<ol>
+    <li>Hannah Shapiro</li>
+    <li>Kyle Jason</li>
+    <li>Spencer Bledsoe</li>
+    <li>Cirie Fields</li>
+    <li>Tony Vlachos</li>
+    <li>Aubry Bracco</li>
+</ol>
+<h3>Yavusa Tolu</h3>
+<ol>
+    <li>Adam Klein</li>
+    <li>Scot Pollard</li>
+    <li>Devon Pinto</li>
+    <li>Cydney Gillon</li>
+    <li>Jay Starret</li>
+    <li>Christian Hubicki</li>
+</ol>
+
+
+<h2 id = "day6-status">Tribe Statuses </h2>
+
+<h3>Kalokalo</h3>
+
+<p>On new Kalokalo, the tribe feels good since they have a lot of strong people like Brad, Michaela, Domenick and Ozzy. But everybody on the tribe agrees that if they somehow lose, Ben is going home for two reasons: he is the biggest threat and is the only one on the tribe who wasn’t on original Taleitaki. Ben knows he is being targeted, but he still has an idol, so if worst comes to worst he can play it to save himself. Domenick is happy that he got swapped with Brad, his number one ally. However, Michaela got swapped away from her two allies, Cirie and Cydney and is now scared for their positions in the game. Even though David is on the tribe, who was in the Millennials vs Gen X alliance, that was never a strong alliance and Michaela is not close to David anyways.</p>
+
+
+<h3>Taleitaki</h3>
+
+<p>On new Taleitaki, Hannah and Cirie are the only ones from old Taleitaki, and are most likely on the bottom. However, the old Kalokalo’s like Hannah and Cirie and aren’t targeting them just yet. Tony and Jason are still together, and they are close, even though Tony is closer with Ben. However, despite Tony having the numbers and a really close ally, he is still really paranoid that he will be targeted. Aubry and Spencer are still together, and despite them not voting with each other last vote, Aubry still convinced Spencer to flip and vote Tony. The two still feel good with each other and want to continue working with each other.</p>
+
+
+<h3>Yavusa Tolu</h3>
+
+<p>The brand new tribe, Yavusa Tola is in a interesting position because 3 members are from old Kalokalo and 3 members are from old Taleitaki. Adam gets paranoid, since he is the only winner on the tribe and the biggest threat - but he still has Jay, who is his biggest ally. Jay had a friendly rivalry with Adam last time, but since then they have become really close friends, and Jay is willing to do anything to save Adam from being the target. Christian and Devon are happy to be together; after all the chaos on old Kalokalo, they grew closer, as they were the only ones they could trust since everybody else kept flipping and doing other crazy stuff. Cydney is worried - not only was she separated from her allies, she is now on a tribe with Scot, who Cydney betrayed in Kaoh Rong. Scot is still mad about that and wants to get Cydney out as soon as possible. Everybody can feel the tension between them, making camp life really uncomfortable.</p>
+
+
+
 <h2 id = "day6-immunity">Immunity Challenge </h2>
 
-<p>Taleitaki won again, meaning Kalokalo is going back to tribal council!</p>
-
-<h2 id = "day6-status"> Kalokalo status</h2>
-
-<p> After getting back from the immunity challenge, Kalokalo is bummed that they lost. However, Tony, Ben, Scot, and Jason all feel good since they have the numbers with Spencer and all they have to do is pick off one of the 4 in the minority. Devon and Christian decide that they should just stick together and hope that the majority splits, so they want to keep it simple and put their votes on Tony. However, Zeke is not going to give up that easily. He goes up to the majority alliance and tries to convince them to put their votes on Aubry, since she is a huge strategic threat. Aubry also doesn’t want to give up - and she realizes that Spencer isn’t fully on board with the alliance. She goes to have a talk with him and tries to convince him that they can be a duo. Spencer wants to work with Aubry, but he’s not sure if he should flip yet. Meanwhile, Ben, Tony, Jason, and Scot realize that Spencer has been talking to Aubry for a long time and they consider putting votes on him also. Christian and Devon also realize that everybody else is talking, and they get worried that either they are throwing them under the bus or telling them there target. So Christian and Devon consider changing the vote to Ben, or also maybe Zeke since they know Zeke likes to play hard and might be throwing them under the bus.</p>
-
-<p>With a ton more crazy things happening, this is going to be another crazy tribal.</p>
-
-
+<h2 id = "day6-status-2"> Status</h2>
 
 <h2 id = "day6-tribal"> Tribal Council </h2>
 
 <h3> Recap </h3>
-
-<p>Devon, Christian, Zeke, and Aubry were in the minority, planning to vote for Tony and hope something happened. However, Zeke talked to the majority and tried to throw Aubry under the bus. Meanwhile, Aubry was forming a bond with Spencer to try to get him to flip. Ben, Tony, Jason, and Scot realized Spencer was gone for a long time and were considering changing the vote to him. Then, Devon and Christian got paranoid and thought that either Zeke told the majority their plan or threw them under the bus. So they thought about flipping to vote out Ben or Zeke.</p>
-
-<h3> The Votes </h3>
-
-<p>I'll go tally the votes. </p>
-
-<p>If anybody has an immunity idol and wants to play it, now would be the time to do so.</p>
-
-<p>Alright. I'll read the votes.</p>
-
-<p>Aubry</p>
-
-<p>Tony</p>
-
-<p>Spencer</p>
-
-<p>Zeke</p>
-
-<p>Spencer. That's two votes Spencer, one vote Aubry, one vote Tony, and one vote Zeke.</p>
-
-<p>Zeke. That's two votes Zeke, two votes Spencer, one vote Tony and Aubry.</p>
-
-<p>Zeke. </p>
-
-
-<p>Zeke. </p>
-
-<p>Second person voted out of Survivor: Dream All Stars.... Zeke.</p>
-
-<p>
-<img src = "imgs/votes-day-6.png"></img>
-</p></br>
-
-
-
-<h2 id = 'day6-status2'>Status </h2>
-
 
 
 `;
