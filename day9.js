1a4d5c53d63f0933c52a52567cf277140c9797d6
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-09 19:50:03 -0400

    Update

---
+++ day9.js
@@ -0,0 +1,31 @@
+
+mystring =  `
+
+<h1 id = "day9">Day 9 </h1>
+
+
+<h2 id = "day9-immunity">Immunity Challenge </h2>
+
+<p>Shocking nobody, Kalokalo has won immunity!</p>
+
+
+<h2 id = "day9-status">Tribe Statuses </h2>
+
+<h3>Taleitaki status</h3>
+
+<p>After the chaoticness at last tribal, Aubry went from being the swing vote and on the top with Spencer, to now being on the bottom. Tony and Jason are mad at her for attempting to flip and want to take her out. Aubry knows this, but luckily she has a good relationship with Cirie, they can take out Tony if they get Hannah on their side. However, Hannah isn’t really allies with Cirie and is willing to flip. The vote will come down to what Hannah decides. </p>
+
+
+<h3>Yavusa Tolu</h3>
+
+<p>On Yavusa Tolu, it is full out war between Cydney and Scot and each of them are trying to get allies on their side. Despite this, Adam is still very paranoid for being the only winner on the tribe and goes idol hunting. He searches for a while…. And finds the idol! He now feels very good and in control. There are two main alliances on the tribe - Devon and Christian, and Jay and Adam. These alliances will decide who goes home. </p>
+
+
+
+<h2 id = "day9-tribal"> Tribal Council </h2>
+
+
+`;
+
+
+writeDay(mystring);
