a5b8c94b9882d6672f61b5a2b5544464e149b2a3
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-09 20:05:54 -0400

    Update

--- day9.js
+++ day9.js
@@ -21,8 +21,42 @@ mystring =  `
 <p>On Yavusa Tolu, it is full out war between Cydney and Scot and each of them are trying to get allies on their side. Despite this, Adam is still very paranoid for being the only winner on the tribe and goes idol hunting. He searches for a while…. And finds the idol! He now feels very good and in control. There are two main alliances on the tribe - Devon and Christian, and Jay and Adam. These alliances will decide who goes home. </p>
 
 
-
 <h2 id = "day9-tribal"> Tribal Council </h2>
+
+<h3> Taleitaki Tribal Council Results </h3>
+
+<p> If anybody has a hidden immunity idol and you want to play it, now would be the time to do so. </p>
+
+<p> I'll read the votes. </p>
+
+<p> First vote....Aubry </p>
+
+<p> Tony. </p>
+
+<p> Aubry. </p>
+
+<p> Tony. We're tied. Two votes Aubry, two votes Tony, one vote left. </p>
+
+<p> Fourth person voted out of Survivor: Dream All Stars.....Tony. </p>
+
+<p> Aubry, Cirie and Hannah voted for Tony. Tony and Jason voted for Aubry. </p>
+
+<h3> Yavusa Tolu Tribal Council Results </h3>
+
+<p> If anybody has a hidden immunity idol, and you want to play it now would be the time to do so. </p>
+
+<p> I'll read the votes. </p>
+
+<p> First vote....Scot </p>
+
+<p> Cydney. </p>
+
+<p> Scot. </p>
+
+<p> Fifth person voted out of Survivor: Dream All Stars... Scot. </p>
+
+<p> Everybody voted for Scot besides himself. </p>
+
 
 
 `;
