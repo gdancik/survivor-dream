e48efd0ac78ff8be3d4e315999070ffc93b73c9f
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-11 21:02:27 -0400

    Update

---
+++ day11.js
@@ -0,0 +1,31 @@
+
+mystring =  `
+
+<h1 id = "day11">Day 11 </h1>
+
+
+<h2 id = "day11-immunity">Immunity Challenge </h2>
+
+
+<p>Kalokalo and Taleitaki have won immunity, so Yavusa Tolu is going to tribal! </p>
+
+
+
+<h2 id = "day11-status">Tribe Statuses </h2>
+
+
+<h3>Yavusa Tolu status</h3>
+<p>After voting out Scot unanimously everybody on the tribe was feeling good so they are sad to have to vote out someone. Devon, Christian, and Cydney all form together to have an easy vote for Adam. They want to split the votes, but it could be too risky because it might end in a tie and then it would go to rocks. In order for this to work, nobody can tell Jay and Adam or else they will get screwed if one of them has an idol, and both of them found idols in Millennials vs Gen X. Devon goes to Adam and Jay and talks with them about voting out Cydney, since she is the biggest threat. She's good in challenges come the merge, and she is pretty strategic. He says he also has Christian so they can all blindside her unanimously. Adam buys it, but is still VERY paranoid.</p>
+
+<p>The question is, will Adam play the idol or not?</p>
+
+
+<h2 id = "day11-tribal"> Tribal Council </h2>
+
+<h3> Results </h3>
+
+
+`;
+
+
+writeDay(mystring);
