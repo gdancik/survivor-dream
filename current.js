6e4e90e9f95e75df896e07c11ad4b9bcfb9816bd
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-12 09:17:31 -0400

    Update

---
+++ current.js
@@ -0,0 +1,52 @@
+
+mystring =  `
+
+<h1 id = "current">Current Status </h1>
+
+<h2> Before tribe swap</h2>
+
+<p> The castaways eliminated are: </p>
+<ol>
+<li> Tai </li>
+<li> Zeke </li>
+</ol>
+
+<h2> Tribe swap on day 6 </h2>
+
+<p> Current status: </p>
+
+<h3>Kalokalo</h3>
+<ol>
+<li>Brad Culpepper</li>
+    <li>David Wright</li>
+    <li>Ben Driebergen</li>
+    <li>Michaela Bradshaw</li>
+    <li>Domenick Abbate</li>
+    <li>Ozzy Lusth</li>
+</ol>
+
+<h3>Taleitaki</h3>
+<ol>
+    <li>Hannah Shapiro</li>
+    <li>Kyle Jason</li>
+    <li><del>Spencer Bledsoe</del></li>
+    <li>Cirie Fields</li>
+    <li><del>Tony Vlachos</del></li>
+    <li>Aubry Bracco</li>
+</ol>
+<h3>Yavusa Tolu</h3>
+<ol>
+    <li>Adam Klein</li>
+    <li><del>Scot Pollard</del></li>
+    <li>Devon Pinto</li>
+    <li><del>Cydney Gillon</del></li>
+    <li>Jay Starret</li>
+    <li>Christian Hubicki</li>
+</ol>
+
+
+
+`;
+
+
+writeDay(mystring);
