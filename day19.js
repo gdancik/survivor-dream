d8d6d8eb5f2cba24b17134177f3f52c9f4782535
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-19 11:15:30 -0400

    Update

---
+++ day19.js
@@ -0,0 +1,23 @@
+
+mystring =  `
+
+<h1 id = "day19">Day 19 </h1>
+
+<h2 id = "day19-immunity">Immunity Challenge </h2>
+
+<p>Come on in guys, let's get to today's immunity challenge. This challenge is called A Bit Tipsy. You must stack blocks in a tower on a wobbly platform. You will have to pull on a rope to keep the table balanced while retrieving your blocks. The first person to stack their blocks and spell "immunity" wins immunity.</p>
+
+<h3 id = "day19-status">  STATUS </h3>
+
+
+<h2 id = "day19-tribal"> Tribal Council </h2>
+
+<h3> Results </h3>
+
+
+
+
+`;
+
+
+writeDay(mystring);
