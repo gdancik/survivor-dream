ca552bfad81e06db1d2bcaf4f65cf40f1ce7dd77
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-19 23:46:31 -0400

    Update

--- day19.js
+++ day19.js
@@ -7,13 +7,53 @@ mystring =  `
 
 <p>Come on in guys, let's get to today's immunity challenge. This challenge is called A Bit Tipsy. You must stack blocks in a tower on a wobbly platform. You will have to pull on a rope to keep the table balanced while retrieving your blocks. The first person to stack their blocks and spell "immunity" wins immunity.</p>
 
-<h3 id = "day19-status">  STATUS </h3>
+<p> Hannah has won immunity! </p>
 
+<h3 id = "day19-status">  Merge Tribe Status </h3>
+
+<p>After two crazy blindsides last night, a lot of groups are now fractured. Christian is mad at Domenick for betraying Aubry, and Cirie is now seperate from Michaela. 
+Since Hannah won immuntiy, Adam wants to target David, and Hannah and David continue trying to take out Adam. They try to get Domenick and Christian, but the two are going against each other after last vote, and want everybody to vote with them. Hannah and David then go to Brad but Brad doesn't want to help anybody and doesn't trust anyone. Hannah wants to split the votes between Brad and Adam because of this, but David would rather split the votes between Jay and Adam, and then has a small fight with Hannah about it. </p>
 
 <h2 id = "day19-tribal"> Tribal Council </h2>
 
 <h3> Results </h3>
 
+<p> I'll go tally the votes. </p>
+
+<p> If anyone has a hidden immunity idol, now would be the time to play it. </p>
+
+<p> Christian: You know what Jeff, with all the chaos going on tonight I might need this. </p>
+
+<p> This is a hidden immunity idol, any votes cast for Christian will not count. I'll read the votes. </p>
+
+<p> First vote...Christian. Does not count. </p>
+
+<p> Christian. Does not count. </p>
+
+<p> David. </p>
+
+<p> Domenick. </p>
+
+<p> Adam. </p>
+
+<p> Brad. </p>
+
+<p> David. That's two votes David, one vote Domenick, one vote Adam, one vote Brad, one vote left. </p>
+
+<p> Final vote...Domenick. We have a tie between Domenick and David. Here's how this is going to work. In the Survivor rules, if the vote is a deadlock we will go to rocks. Domenick and David, since you are in the tie you are safe. Christian played an idol so he is safe. Hannah has immunity so she is safe. The rest of you - Adam, Brad, Jay, Cirie - you will draw rocks. If you draw a white rock you are safe. If you draw a black rock you are eliminated. </p>
+
+<p> For the second time this season, let's draw rocks. </p>
+
+<p> The person who draws the black rock is.... </p>
+
+</br></br>
+
+<p> Jay. </p>
+
+
+<p>
+<img src = "imgs/votes-day-19.png"></img>
+</p></br>
 
 
 
