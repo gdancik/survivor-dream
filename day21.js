52ca1eb0de40cf41865d3f0b911816d5418fed91
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-21 23:19:15 -0400

    Update

---
+++ day21.js
@@ -0,0 +1,59 @@
+
+mystring =  `
+
+<h1 id = "day21">Day 21 </h1>
+
+<h2 id = "day21-events"> Loved Ones Visit and Reward Challenge </h2>
+
+<h3> Loved Ones </h3>
+
+<p> Alright guys, let's get to todays reward challenge. It's time for some love. </p>
+
+<p> David, here's your dad Doug. </p>
+
+<p> Brad, here's your wife Monica. </p>
+
+<p> Adam, here's your brother Evan. </p>
+
+<p> Hannah, here's your mom Liz. </p>
+
+<p> Domenick, here's your wife Kristin. </p>
+
+<p> Christian, here's your girlfriend Emily. </p>
+
+<h3> Reward Challenge </h3>
+
+<p>Alright. Let's to get to the reward challenge. You will be split into two teams of three. The winning team will have to pick two people from the losing team to join them. The person not picked will have to go back to camp all by themselves. Here are the teams: </p>
+
+<h4>Team 1</h4>
+<ol>
+    <li>Christian</li>
+    <li>Hannah</li>
+    <li>Domenick</li>
+</ol>
+
+<h4>Team 2</h4>
+<ol>
+    <li>Brad</li>
+    <li>David</li>
+    <li>Adam</li>
+</ol>
+
+
+
+<h2 id = "day21-immunity">Immunity Challenge </h2>
+
+
+<h3 id = "day21-status">  Tribe Status </h3>
+
+
+<h2 id = "day21-tribal"> Tribal Council </h2>
+
+<h3> Results </h3>
+
+
+
+`;
+
+
+writeDay(mystring);
