686cc89dce56357d5b0c3071580eb71da1c93a21
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-24 20:18:52 -0400

    Update

---
+++ day24.js
@@ -0,0 +1,27 @@
+
+mystring =  `
+
+<h1 id = "day24">Day 24 </h1>
+
+<h2 id = "day24-immunity">Immunity Challenge </h2>
+
+<p> Alright, let's get to the immunity challenge. This challenge is called You Shook Me. You must stack letter blocks on a wobbly platform. The first person to spell "Dream", "All", and "Stars" without any upside down letters and lock in their mechanism wins. 
+This challenge was the iconic final immunity challenge of Survivor: Heroes vs Healers vs Hustlers, where Ben famously put an upside U. </p>
+
+<p>Whoever wins this challenge will be able to pick someone to go with them to the final three, the other two will have to compete in fire.</p>
+
+<p>Hannah has won immunity! </p>
+
+
+<h2 id = "day24-status">Status </h2>
+
+<p>She now has to pick one person to go to the final three with her. She is suspicious that there might not be a final three, considering that there is still two days left after this one. She knows that Domenick and Adam are the biggest threats, but she wants Adam out more, as Domenick has made some people mad, while Adam is impressing the whole entire jury. Also, she knows Domenick isn't that good at fire. He's never made fire the whole time, and in Ghost Island he lost the game because he didn't give up immunity to go against Wendell, which caused him to lose the game. This shows that he isn't really good at fire. Hannah and Adam make the fire the most at camp, so Hannah thinks she is the only one who has a chance to take out Adam. She could give up immunity to go against Adam and possible go out in a really dumb way, but if she doesn't do it, Adam would still be in the final three. What will she do? </p>
+
+<h2 id = "day24-tribal"> Tribal Council </h2>
+
+<h3> Results </h3>
+
+`;
+
+
+writeDay(mystring);
