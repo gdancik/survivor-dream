a3297a0fd561924e34bcb0c3e8052bb6b301fd9b
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-26 11:52:42 -0400

    Update

--- day25.js
+++ day25.js
@@ -7,6 +7,8 @@ mystring =  `
 
 <p>Alright everybody, welcome to your final immunity challenge! This immunity challenge is quite iconic and has been used in many seasons. It is called Simmotion. Each of you must drop a ball into a metal chute. The ball will travel down a spiraling metal track and exits out of one of two points (a turnstile gate in the middle of the track causes the balls to come out of alternate exits). You must catch the ball and drop it back into the chute at the top. At designated intervals, an additional ball is added to the track until seven balls are simultaneously in play. If a ball exits the track and is not caught at any time, you are eliminated. Last person standing wins. Big stakes, lets get to it.</p>
 
+<p>Adam has won immunity! Who will he bring to the final two?</p>
+
 
 <h2 id = "day25-status">Status </h2>
 
