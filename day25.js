c73c2891b072cc456a05aff92cfa1f0d9657cc3c
Garrett Dancik <garrett.dancikg@gmail.com>
2023-07-27 11:45:36 -0400

    Update

--- day25.js
+++ day25.js
@@ -11,43 +11,7 @@ mystring =  `
 
 <p> Adam will bring Hannah! </p>
 
-<h2 id = "day25-tribal"> Tribal Council </h2>
 
-<p> Alright, now the power will shift to the jury. </p>
-
-<p> *Jury Votes* </p>
-
-<p> Alright, it's been a great season. I'll see you in Los Angelos. </p>
-
-<h3> Results </h3>
-
-<p> Back at the reunion, Jeff walks in with the votes. </p>
-
-<p> For the last time, I will read the votes. </p>
-
-<p> First vote...Hannah. </p>
-
-<p> Adam. </p>
-
-<p> Hannah. That's two votes Hannah. </p>
-
-<p> Adam. We're tied. Two votes Hannah, two votes Adam. </p>
-
-<p> Hannah. </p>
-
-<p> Adam. We're still tied. Three votes Hannah, three votes Adam. </p>
-
-<p> Hannah. </p>
-
-<p> Adam. Tied once again. Four votes Hannah, four votes Adam. </p>
-
-<p> Adam. </p>
-
-<p> Adam. </p>
-
-<p> Winner of Survivor....ADAM!!! </p>
-
-<p> Well, Adam joins Tony and Sandra in two time winners club. Thank you everybody for another great season of Survivor, we'll see you next season! </p>
 
 `;
 
